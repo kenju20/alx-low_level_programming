3d4c9366d21180752e8b2f7c5fec4508d77f09d7
kenju20 <96546201+kenju20@users.noreply.github.com>
2022-11-02 13:58:52 +0600

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* puts_half - print second half of a string
+* @str: char array string type
+* Description: If odd number of chars, print (length - 1) / 2
+*/
+
+void puts_half(char *str)
+{
+int tupapiesteban;
+
+for (tupapiesteban = 0; str[tupapiesteban] != '\0'; tupapiesteban++)
+;
+
+tupapiesteban++;
+for (tupapiesteban /= 2; str[tupapiesteban] != '\0'; tupapiesteban++)
+{
+_putchar(str[tupapiesteban]);
+}
+_putchar('\n');
+}
